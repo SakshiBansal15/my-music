beb3d0c9631c1a7780bda483ac6790e5c8b44a30
SakshiBansal15 <bansalsakshi1408@gmail.com>
2024-11-24 16:32:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+
+import { useState, useEffect } from "react";
+import Player from "./components/Player";
+
+function App() {
+  const [songs] = useState([
+   
+    {
+      title: "Nach Meri Rani",
+      artist: "Yo Yo Honey Singh",
+      img_src: "./images/nach-meri-rani.jpg",
+      src: "./music_audio/Naach Meri Rani.mp3",
+    },
+    {
+      title: "Care Ni Karda",
+      artist: "Yo Yo Honey Singh",
+      img_src: "./images/care-ni-karda.jpg",
+      src: "./music_audio/Care Ni Karda.mp3",
+    },
+    {
+      title: "Burj Khalifa",
+      artist: "Shashi",
+      img_src: "./images/burjkalifa.jpg",
+      src: "./music_audio/BurjKhalifa.mp3",
+    },
+    {
+      title: "Tango Del Fuego",
+      artist: "Parov Stelar",
+      img_src: "./images/ParovStelarGeorgiaGibbs-TangoDelFuego.jpg",
+      src: "./music_audio/ParovStelarGeorgiaGibbs-TangoDelFuego.mp3",
+    },
+    {
+      title: "Take You Dancing",
+      artist: "Jason Derulo",
+      img_src: "./images/JasonDerulo-TakeYouDancing.jpg",
+      src: "./music_audio/JasonDerulo-TakeYouDancing.mp3",
+    },
+    {
+      title: "Daisy",
+      artist: "Ashnikko",
+      img_src: "./images/Ashnikko-Daisy.jpg",
+      src: "./music_audio/Ashnikko-Daisy.mp3",
+    },
+    {
+      title: "Dolly Song",
+      artist: "Partz Grimbad",
+      img_src: "./images/PatzGrimbard-DollySong.jpg",
+      src: "./music_audio/PatzGrimbard-DollySong.mp3",
+    },
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex, songs.length]);
+
+  return (
+    <div className="App">
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
+      />
+    </div>
+  );
+}
+
+export default App;
